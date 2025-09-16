3c79a3c9391a1378e924ba9246da3d754cdb8152
Thomas Boel Micheelsen <tbmi@ganintegrity.com>
2025-09-16 22:00:16 +0200

    feat: improve service import with inquirer

--- src/commands/service/import.ts
+++ src/commands/service/import.ts
@@ -1,43 +1,82 @@
 import { Command } from '@commander-js/extra-typings';
+import inquirer from 'inquirer';
 
 import store, { Service } from '../../utils/store.js';
 import { withErrorHandler } from '../../utils/errorHandler.js';
 import { addService } from './add.js';
 import { remove } from './remove.js';
+import { padLabel } from '../../utils/prettyPrintKeyValue.js';
+
+type ServiceImport = Service & { cliVersion: string };
 
 export async function _import(
   servicesImport: string,
   { force }: { force: boolean },
 ): Promise<void> {
-  const services = store.get('services') ?? [];
+  const serviceImports: ServiceImport[] = JSON.parse(servicesImport);
 
-  const servicesToAdd: Service[] = JSON.parse(servicesImport);
+  console.clear();
 
-  for (const serviceToAdd of servicesToAdd) {
-    const serviceAlreadyExists = services.some(
-      (service) => service.name === serviceToAdd.name,
+  const { choices }: { choices: string[] } = await inquirer.prompt([
+    {
+      type: 'checkbox',
+      name: 'choices',
+      message: 'Select the services you wish to import',
+      choices: serviceImports.map(
+        (s) =>
+          `${padLabel('name: ' + s.name, 40)} port: ${padLabel(s.port?.toString() ?? 'N/A', 5)} - repo: ${s.repository}`,
+      ),
+      pageSize: 40,
+      loop: false,
+    },
+  ]);
+
+  console.clear();
+
+  const servicesToImport = choices
+    .map((choice) =>
+      serviceImports.find((service) =>
+        choice.startsWith(`name: ${service.name}`),
+      ),
+    )
+    .filter((service) => service !== undefined);
+
+  const existingServices = store.get('services') ?? [];
+
+  for (const serviceToImport of servicesToImport) {
+    const serviceAlreadyExists = existingServices.some(
+      (service) => service.name === serviceToImport.name,
     );
 
     if (serviceAlreadyExists && !force) {
-      console.warn(
-        `ℹ️ Skipping import of service with the name "${serviceToAdd.name}" since it already exists. Use --force to force an overwrite of the existing service.`,
-      );
-      continue;
+      const { confirmed } = await inquirer.prompt([
+        {
+          type: 'confirm',
+          name: 'confirmed',
+          message: `A service with the name "${serviceToImport.name}" already exists, do you wish to overwrite the existing service?`,
+        },
+      ]);
+
+      if (confirmed) {
+        await remove(serviceToImport.name);
+      } else {
+        continue;
+      }
     }
 
     if (serviceAlreadyExists && force) {
-      await remove(serviceToAdd.name);
+      await remove(serviceToImport.name);
     }
 
     await addService({
-      name: serviceToAdd.name,
-      type: serviceToAdd.type,
-      runCommand: serviceToAdd.runCommand,
-      ...(serviceToAdd.alias ? { alias: serviceToAdd.alias } : {}),
-      ...(serviceToAdd.port ? { port: serviceToAdd.port } : {}),
-      expectedSecondsToStart: serviceToAdd.expectedSecondsToStart,
-      repository: serviceToAdd.repository,
-      runtime: serviceToAdd.runtime,
+      name: serviceToImport.name,
+      type: serviceToImport.type,
+      runCommand: serviceToImport.runCommand,
+      ...(serviceToImport.alias ? { alias: serviceToImport.alias } : {}),
+      ...(serviceToImport.port ? { port: serviceToImport.port } : {}),
+      expectedSecondsToStart: serviceToImport.expectedSecondsToStart,
+      repository: serviceToImport.repository,
+      runtime: serviceToImport.runtime,
     });
   }
 }
